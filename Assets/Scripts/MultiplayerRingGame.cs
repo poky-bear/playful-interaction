fa2f9629d10f279cca6b2d70a1551daf30219fd5
Sara <helenyuzhang@gmail.com>
2025-04-15 22:44:45 -0400

    Revert "Fixed 'currentAttemptSuccessful' variable scope issue"
    This reverts commit a08ba4abdbaca1d70e0b101919f2fe5da2d94e30.

--- Assets/Scripts/MultiplayerRingGame.cs
+++ Assets/Scripts/MultiplayerRingGame.cs
@@ -809,7 +809,7 @@ public class MultiplayerRingGame : MonoBehaviour
         {
             // Only one player has attempted so far
             // Provide feedback on how close they were
-            if (player1Success || player2Success)
+            if (currentAttemptSuccessful)
             {
                 Debug.Log("Multiplayer: Good hit! Waiting for the other player...");
                 
